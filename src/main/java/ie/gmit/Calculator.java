61b2ba4d5b225575f9fe62b65ab1022f102b9366
Aonghus <g00293306@gmit.ie>
2022-02-24 10:05:26 +0000

    First commit in my calculator repository

---
+++ src/main/java/ie/gmit/Calculator.java
@@ -0,0 +1,14 @@
+package ie.gmit;
+
+public class Calculator {
+
+
+    int add(int firstNum, int secondNum) {
+        int sum;
+        sum = firstNum + secondNum;
+        return sum;
+    }
+
+
+}
+
